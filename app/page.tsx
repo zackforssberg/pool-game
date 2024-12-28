dc497bd510214ea5c81a7a1a84f37e00fcdc345f
Zack Forssberg <zack.forssberg@iths.se>
2024-12-28 13:50:25 +0100

    fixed issue with darkmode

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function PoolGame() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-2xl font-bold mb-4">Last Ball Standing</h1>
       <Link className="mb-4" href="/rules">
         <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300">
--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function RulesPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen p-6">
       <h1 className="text-3xl font-bold mb-4 text-center">
         How to Play <br /> Last Ball Standing
       </h1>
-      <div className="bg-white rounded-md shadow-md p-6 max-w-3xl text-lg space-y-4">
+      <div className="rounded-md shadow-md p-6 max-w-3xl text-lg space-y-4">
         <p>
           Welcome to <span className="font-bold">Last Ball Standing</span>! This
           fun and engaging game is designed for 2 to 10 players. Each player
